c944983b9a77096405a8290a9080d3664d44f63f
FlorianSahbi <florian.sahbi@gmail.com>
2019-07-16 16:55:22 +0200

    add event websocket in every request and cors

--- index.js
+++ index.js
@@ -36,9 +36,25 @@ const matches = [{
     sport: 'tennis',
     location: 'lausanne',
     competition: 'WTA Tour'
-},]
-
-app.get('/api/controls/:actionId', function (req, res) {
+}, {
+    matchId: 2,
+    title: 'Lorem ipsum dolor',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices nec nulla ut vulputate. Duis ut turpis vel felis scelerisque lobortis. Maecenas sed felis quis leo interdum sollicitudin nec placerat neque. Phasellus tempor massa quis scelerisque dapibus. Donec egestas, sapien nec pulvinar rhoncus, nulla orci blandit lacus, vel euismod massa nisi ac urna. Integer eget ipsum a nisi varius euismod. Sed cursus ligula vitae arcu vestibulum, sed lacinia metus commodo.',
+    players: [{
+        firstName: 'conny',
+        lastName: 'perrin',
+        country: 'SU'
+    }, {
+        firstName: 'natalia',
+        lastName: 'vikhlyantseva',
+        country: 'RU'
+    }],
+    sport: 'tennis',
+    location: 'lausanne',
+    competition: 'WTA Tour'
+}]
+app.use(cors());
+app.get('/api/controls/:actionId', (req, res) => {
 
     const { actionId } = req.params;
 
@@ -49,24 +65,42 @@ app.get('/api/controls/:actionId', function (req, res) {
         actionId
     }
 
-    switch (action) {
-        case 0:
-            socket.emit('play', { action, description: 'Play' });
+    switch (actionId) {
+        case '0':
+            console.log({ event: 'Play' });
+            io.emit('play', { action: actionId, description: 'Play' });
             break;
 
-        case 1:
-            socket.emit('pause', { action, description: 'Pause' });
+        case '1':
+            console.log({ event: 'Pause' });
+            io.emit('pause', { action: actionId, description: 'Pause' });
             break;
 
-        case 2:
-            socket.emit('random', { action, description: 'Random' });
+        case '2':
+            console.log({ event: 'Random' });
+            io.emit('random', { action: actionId, description: 'Random' });
+            break;
+
+        case '3':
+            console.log({ event: 'zoomIn' });
+            io.emit('zoomIn', { action: actionId, description: 'zoomIn' });
+            break;
+
+        case '4':
+            console.log({ event: 'zoomOut' });
+            io.emit('zoomOut', { action: actionId, description: 'zoomOut' });
+            break;
+
+        case '5':
+            console.log({ event: 'switchTeam' });
+            io.emit('switchTeam', { action: actionId, description: 'switchTeam' });
             break;
     }
 
     res.status(200).json(response);
 });
 
-app.get('/api/camera/:cameraId', function (req, res) {
+app.get('/api/camera/:cameraId', (req, res) => {
 
     const { cameraId } = req.params;
 
@@ -76,11 +110,12 @@ app.get('/api/camera/:cameraId', function (req, res) {
         url: `/api/camera/${cameraId}`,
         cameraId
     }
-
+    console.log({ event: `Show camera ${cameraId}` })
+    io.emit('play', response);
     res.status(200).json(response);
 });
 
-app.get('/api/auth/:pseudo', function (req, res) {
+app.get('/api/auth/:pseudo', (req, res) => {
 
     const { pseudo } = req.params;
 
@@ -106,25 +141,59 @@ app.get('/api/auth/:pseudo', function (req, res) {
 
 });
 
-app.get('/api/matches/', function (req, res) {
-    res.status(401).json(matches);
+app.get('/api/matches/', (req, res) => {
+
+    const response = {
+        matches: matches[matchId],
+        message: `Request has been handled sucessfully`,
+        code: 200,
+        url: `/api/matches`,
+    }
+
+    console.log({ event: `Show matches` })
+    io.emit('show current match', matches);
+    res.status(200).json(response);
 });
 
-app.get('/api/match/:matchId', function (req, res) {
+app.get('/api/match/:matchId', (req, res) => {
 
     const { matchId } = req.params;
+
+    const response = {
+        match: matches[matchId],
+        message: `Request has been handled sucessfully`,
+        code: 200,
+        url: `/api/match/${matchId}`,
+    }
 
     if (matches.length - 1 < matchId) {
         res.status(404).json({ message: `Error : This match doesnt exists` })
     } else if (matchId < 0) {
         res.status(404).json({ message: `Error : Can't use negative numbers` })
     } else {
-        res.status(200).json({ match: matches[matchId] });
+        console.log({ event: `Show match ${matchId}` })
+        io.emit('show match', response);
+        res.status(200).json(response);
     }
 });
 
-io.on('connection', function (socket) {
+app.get('/api/match/', (req, res) => {
+    let random = Math.floor(Math.random() * ((matches.length - 1) - 0) + 0);
+
+    response = {
+        match: matches[random],
+        message: `Request has been handled sucessfully`,
+        code: 200,
+        url: `/api/match`,
+    }
+
+    console.log({ event: `Show current match` })
+    io.emit('show current match', response);
+    res.status(200).json(response)
+});
+
+io.on('connection', (socket) => {
     console.log('a user connected');
 });
 
-http.listen(process.env.PORT || 3100)
+http.listen(process.env.PORT || 3100);
